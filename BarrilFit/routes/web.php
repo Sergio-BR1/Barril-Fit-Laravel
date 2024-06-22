dc20230fcf444c76be1244ed8618e6da7c4207a9
Sérgio Brito <71470010+Sergio-BR1@users.noreply.github.com>
2024-06-21 23:36:10 -0300

    Delete BarrilFit directory

--- BarrilFit/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\InicioController;
-use App\Http\Controllers\FichaController;
-
-Route::resource('/', InicioController::class);
-
-Route::resource('/ficha', FichaController::class);
