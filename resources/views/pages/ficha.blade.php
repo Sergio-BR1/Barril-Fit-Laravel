dbd324aef4f984b783d043e55653c3bc67aa3ae5
Sérgio Brito <mariosbps@ufba.br>
2024-09-12 10:24:23 -0300

    (refactor): view ficha inherit template

--- resources/views/pages/ficha.blade.php
+++ resources/views/pages/ficha.blade.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
-    <script src="/assets/scripts/script.js"></script>
-    <link rel="shortcut icon" href="/assets/images/favicon-32x32.png" type="image/x-icon" />
-
-    <title>Barril Fit</title>
-</head>
-<body>
-
+@extends('layouts.default')
+@section('content')
 
    <!-- Estrutura principal do formulário -->
 <div class="container-ficha">
@@ -112,7 +100,5 @@
 
 
 
-
-</body>
-</html>
+@stop
 
